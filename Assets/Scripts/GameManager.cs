25af90071fe075516077806e743ebc010619d8cc
xSpr0ut <Joellaxlin@gmail.com>
2025-11-09 12:36:21 -0500

    Map layout done for level1, coding begins

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool isBlueBoxIn = false;
+    private bool isGreenBoxIn = false;
+    private bool isRedBoxIn = false;
+    void Update()
+    {
+        
+    }
+}
